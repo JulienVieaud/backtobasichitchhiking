6a4ab2c9143cd78f08e9f6389da5f94f1619e0ff
Formation <Formation@M2i-JAV6-16>
2018-01-11 11:39:45 +0100

    mise en place du test de validité des input correction de la function test si existe correction vérification si email

--- TP-OMDB-routing/creer_compte.js
+++ TP-OMDB-routing/creer_compte.js
@@ -1,53 +1,47 @@
-
-
 function creerCompte(){
-	var autorisationLogin = 1;
-	var autorisationPassword = 1;
+
+	var autorisationPassword = true;
 	var login=document.getElementById("login").value;
 	var password=document.getElementById("password").value;
 
-	if (login == "") {
-		console.log("Identifiant vide : Veuillez insérer une adresse e-mail valide");
-		autorisationLogin = 0;
-	}
+
 	if (password == "") {
 		console.log("Password vide : Veuillez insérer un mot de passe valide");
-		autorisationPassword = 0;
+		autorisationPassword = false;
 	}
 
-	if (autorisationLogin == 1 && autorisationPassword == 1) {
-		var user={login:login, password:password};
+	if (autorisationPassword == true) {
 
+		var user={login:login, password:password};
 		var listUser=JSON.parse(localStorage.getItem("listUser"));
-		console.log(listUser);
 		if(listUser==null){
 			listUser=[]
 		}
 
-		testerDoublonUser(listUser, user);
+		testerDoublonUser(listUser, user)
 
-		listUser.push(user);
-		localStorage.setItem('listUser',JSON.stringify(listUser));
-		localStorage.setItem('user', user);
-		console.log(listUser);
+		if(testerDoublonUser(listUser, user)==false){
+
+			listUser.push(user);
+			localStorage.setItem('listUser',JSON.stringify(listUser));
+			localStorage.setItem('user', user);
+			console.log(listUser);
+		}
 	}
 }
 
 function testerDoublonUser(listUser, user){
-	var testDoublonUserKO = 0;
+	var testDoublonUserKO = false;
 	for (var i = 0; i < listUser.length; i++) {
 		if (user.login == listUser[i].login){
 			console.log(user.login + " : KO - Compte existe !");
-			testDoublonUserKO = 1;
+			testDoublonUserKO = true;
 			break;
 		}
 	}
-	if (testDoublonUserKO == 0) {
+	if (testDoublonUserKO == false) {
 		console.log(user.login + " : OK - Nouveau compte");
 	}
+
+	return testerDoublonUser;
 }
-
-function testerDonnee(){
-
-}
-
